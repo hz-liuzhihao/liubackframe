ba789eb246d13726521415986dc06bda37396f72
liuzhihao <wb-lzh760330@alibaba-inc.com>
2021-10-09 13:48:33 +0800

    feat: 提交nacos服务注册和发现

--- gateway/src/main/java/com/wecan/GatewayApplication.java
+++ gateway/src/main/java/com/wecan/GatewayApplication.java
@@ -1,11 +1,12 @@
 package com.wecan;
 
-import com.alibaba.nacos.spring.context.annotation.config.NacosPropertySource;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 
 @SpringBootApplication
-@NacosPropertySource(dataId = "gateway", autoRefreshed = true)
+@EnableDiscoveryClient
 public class GatewayApplication {
 
 	public static void main(String[] args) {
