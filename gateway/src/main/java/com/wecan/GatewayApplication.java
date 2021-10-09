06ca8b79478bdae7c6f5df7422c172544e315b16
liuzhihao <wb-lzh760330@alibaba-inc.com>
2021-10-09 11:33:46 +0800

    feat: 增加nacos配置

--- gateway/src/main/java/com/wecan/GatewayApplication.java
+++ gateway/src/main/java/com/wecan/GatewayApplication.java
@@ -1,9 +1,11 @@
 package com.wecan;
 
+import com.alibaba.nacos.spring.context.annotation.config.NacosPropertySource;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@NacosPropertySource(dataId = "gateway", autoRefreshed = true)
 public class GatewayApplication {
 
 	public static void main(String[] args) {
